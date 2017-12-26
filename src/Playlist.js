07c570be05a4d7daf0377b952b3667c126ce0a65
Xuqian Zhang <zxuqian@Xuqians-MBP.fios-router.home>
2017-12-25 19:44:40 -0500

    --

--- src/Playlist.js
+++ src/Playlist.js
@@ -4,7 +4,85 @@ class Playlist extends Component {
     render() {
         return (
             <div className="Xq-Playlist">
-                <h1>Placeholder....</h1>
+                <div className="thead">
+                    <div className="th">Title</div>
+                    <div className="th">Artist</div>
+                    <div className="th">Album</div>
+                    <div className="th">Date Added</div>
+                    <div className="th">Duration</div>
+                </div>
+                <div className="tbody">
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                    <div className="row">
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                        <div className="col">Test</div>
+                    </div>
+                </div>
             </div>
         )
     }
